a6f838853c0fd20196023ed8cde6e26555a6ac8a
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 18:48:07 +0200

    Update fourth_task.c

--- fourth_task.c
+++ fourth_task.c
@@ -1,5 +1,32 @@
 /* Task description:
+Write a program that contains an array of 10 elements initialized by values of your choice. (pls check the homepage too)
 
+Shift all elements in the array towards the beginning of the array by one cell. 
+The first element should go into the last cell (you may call this process rotation). 
+Repeat the shifting (rotation) process 10 times, printing the array after each step. Your output should be similar to the one shown on the right side.
+
+25 69 54  8 77  6 29 10  3 98
+69 54  8 77  6 29 10  3 98 25
+54  8 77  6 29 10  3 98 25 69
+ 8 77  6 29 10  3 98 25 69 54
+77  6 29 10  3 98 25 69 54  8
+ 6 29 10  3 98 25 69 54  8 77
+29 10  3 98 25 69 54  8 77  6
+
+Keep in mind, that the task is not to obtain the output by using tricky indexing! The elements of the array must actually move around!
+
+LOOP
+
+    print the array<---only printf here, no change in the array
+
+    rotate the array to the left by 1<--- the array changes, no printf here
+
+END OF LOOP
+
+Hint
+
+There is no need for a second array! The second element can overwrite the first, the third can overwite the second… 
+The only qestion is what shall we put into the last cell. Make a drawing and find out the necessary steps.
 
 */
 
@@ -7,7 +34,7 @@
 #include <stdio.h>
 
 int main(){
-
+int array[10] = {25, 69, 54, 8, 77, 6, 29, 10, 3, 98};
 
   return 0;
 }
