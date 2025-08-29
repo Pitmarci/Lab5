39dd896a63a2f69823d23a2cc2005a8db4eadd68
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 18:50:41 +0200

    Update fifth_task.c

--- fifth_task.c
+++ fifth_task.c
@@ -1,5 +1,12 @@
 /* Task description:
+An automatic speed control unit on a highway delivers data of the speeding cars only for several days in the following format: „hours minutes speed”. For example 12 45 198 means that at 12:45 some car passed by at a speed of 198 km/h. Data is terminated by 0 0 0.
 
+Write a C program that reads the data (several hours-minutes-speed triplets, until the terminating 0-0-0 triplet) and prints a table with the fastest speeding car in every hour! If there was no car violating the speed limit in a given hour, exclude that interval from the output. The output should be like:
+
+14:00-14:59 -> 145 km/h
+16:00-16:59 -> 167 km/h
+
+(Hint: all speed values are positive.)
 
 */
 
